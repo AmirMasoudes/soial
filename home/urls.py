edc6b2e0347e9061cd11c10d793cfee7a35222a5
amir <amirmasoud.es80@gmail.com>
2023-07-08 00:07:54 +0330

    k

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('post/<int:post_id>/<slug:post_slug>/',views.PostDetailView.as_view(), name='post_detail'),
     path('post/delete/<int:post_id/', views.PostDeleteView.as_view(), name='post_delete'),
     path('post/delete/<int:post_id', views.PostUpdateViwe.as_view(),name='post_update'),
-    ]
+]
